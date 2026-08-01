caffb09c189897a1afcdfac5e5d81facc5b98b1c
Raven <Greyvira777@gmail.com>
2026-07-31 23:36:29 -0400

    Update ZZZZ_KH1FM_Equipment_Stats_Abilities_LIMIT_RISK_v2_2.lua

--- scripts/ZZZZ_KH1FM_Equipment_Stats_Abilities_LIMIT_RISK_v2_2.lua
+++ scripts/ZZZZ_KH1FM_Equipment_Stats_Abilities_LIMIT_RISK_v2_2.lua
@@ -207,7 +207,7 @@ local KEYBLADES = {
     ["Lionheart"]        = GEAR({}),
     ["Metal Chocobo"]    = GEAR({}),
     ["Oathkeeper"] = GEAR({
-        HP = 100, MP = 20, STR = 4, DEF = 1,
+        HP = 5, MP = 5, STR = 5, DEF = 5, ICE_RESISTANCE =+100, LIMIT =+20, RISK=+40
         ABILITIES = { "MP Haste" },
     }),
     ["Oblivion"]         = GEAR({}),
